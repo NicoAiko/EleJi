bf63f37b685b0b40e9e60e35488734baff0342b0
Nico <nicochan95.anime@gmail.com>
2017-02-04 22:10:31 +0100

    Add basic i18n for multi-lingual purposes

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 const {
   app,
-  BrowserWindow
-} = require('electron')
-const path = require('path')
-const url = require('url')
+  BrowserWindow,
+  ipcMain
+} = require('electron');
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     width: 800,
     height: 600
-  })
+  });
 
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   // Open the DevTools.
   // win.webContents.openDevTools()
@@ -32,13 +33,13 @@ function createWindow() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null
-  })
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -47,7 +48,7 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
@@ -55,7 +56,12 @@ app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+ipcMain.on("translation-complete", (event, args) => {
+  // This will show the sender's BrowserWindow
+  BrowserWindow.fromWebContents(event.sender.webContents).show();
+});
---
+++ translate.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+  const handlebars = require("handlebars");
+  const i18n = require("i18n");
+  const ipcRenderer = require("electron").ipcRenderer;
+
+  //Setting up i18n. In this example English and German are supported. Translation files
+  //are put in subfolder "locales"
+  i18n.configure({
+    locales:['en', 'de'],
+    directory: __dirname + '/locales'
+  });
+
+  //This helper makes the connection between Handlebars and I18n
+  //Here German is selected. In a real world application the user should of course be
+  //able to choose this.
+  handlebars.registerHelper('i18n', function(str){
+    return i18n.__({phrase:str, locale:"de"});
+  });
+
+  //This replaces the entire document with the translation
+  var template = handlebars.compile(document.documentElement.innerHTML);
+  document.documentElement.innerHTML = template();
+
+  //Notify main process that the translation is complete
+  ipcRenderer.send("translation-complete", null);
+};
