701fa02472112d043ad7651c4bcab1876aca6b99
NicoAiko <nicochan95.anime@gmail.com>
2018-01-27 19:06:37 +0100

    Update project to electron-vue project

---
+++ src/renderer/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import axios from 'axios';
+
+import App from './App';
+import router from './router';
+import store from './store';
+
+import locale from './locales/locale';
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
+Vue.http = Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
+
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: window.navigator.language,
+  fallbackLocale: 'en',
+  messages: locale,
+});
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  i18n,
+  template: '<App/>',
+}).$mount('#app');
